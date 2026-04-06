a41b8ce09c8dc81e922a582ec0bb3e1409d91fc4
Kalyan Singh <webkalyan334@gmail.com>
2026-04-06 15:31:00 +0530

    issue check

--- src/data/projects.js
+++ src/data/projects.js
@@ -127,7 +127,7 @@ const projects = [
      client: "Travel Booking Website",
      technologies: "HTML, CSS, wordpress",
      duration: "5 + Days",
-     status: "panding..",
+     status: "pending",
      url: "https://triundhikers.com/",
      description: "Responsive marketing website with modern UI."
    },
@@ -155,59 +155,6 @@ const projects = [
      url: "https://www.fitandfabwithswati.com/",
      description: "Responsive marketing website with modern UI."
    }
-
-
-  // {
-  //   id: 2,
-  //   title: "Digital Marketing",
-  //   category: "HTML / CSS",
-  //   images: [kad2, kad],
-  //   client: "Digital Marketing Website",
-  //   technologies: "HTML, CSS, Bootstrap, JavaScript",
-  //   duration: "5 Days",
-  //   status: "Completed",
-  //   url: "https://io.pixelsoftwares.com/HTML/kad_infotech/",
-  //   description: "Responsive marketing website with modern UI."
-  // },
-
-  // {
-  //   id: 3,
-  //   title: "Travel Website",
-  //   category: "WordPress",
-  //   images: [wp1, wp2],
-  //   client: "Travel Agency",
-  //   technologies: "WordPress, Elementor",
-  //   duration: "7 Days",
-  //   status: "Completed",
-  //   url: "#",
-  //   description: "Custom WordPress website for travel agency."
-  // },
-
-  // {
-  //   id: 4,
-  //   title: "React Portfolio",
-  //   category: "React JS",
-  //   images: [react1],
-  //   client: "Self Project",
-  //   technologies: "React, Vite, Bootstrap",
-  //   duration: "3 Days",
-  //   status: "Completed",
-  //   url: "#",
-  //   description: "Portfolio website built using React."
-  // },
-
-  // {
-  //   id: 5,
-  //   title: "Fitness App UI",
-  //   category: "Figma (UI/UX)",
-  //   images: [figma1],
-  //   client: "UI Concept",
-  //   technologies: "Figma",
-  //   duration: "2 Days",
-  //   status: "Completed",
-  //   url: "#",
-  //   description: "UI/UX design for fitness mobile app."
-  // }
 ]
 ;
 
